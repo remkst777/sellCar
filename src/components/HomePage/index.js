01da5a2bff569f77528a0f08361e5c3902b2a11c
remkst777 <31829395+remkst777@users.noreply.github.com>
2019-02-16 03:17:52 +0300

    Use React Hooks instead of classes, updated react, react-dom

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
@@ -8,106 +8,101 @@ import { bmw, ferrari, vw, audi, mercedes, anotherCar } from 'routes-config';
 import { confirmEmail } from 'utils/accountManagement';
 import { Box, Item } from './Box';
 
-// TODO: use react hooks
-class HomePage extends React.PureComponent {
-  componentDidMount() {
-    const { token } = this.props.match.params;
-
-    if (token) {
-      confirmEmail(this.props.match.url);
+const HomePage = ({ match }) => {
+  useEffect(() => {
+    if (match.params.token) {
+      confirmEmail(match.url);
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <title>Home Page</title>
-          <meta name="description" content="Home Page | Description" />
-        </Helmet>
+  return (
+    <div>
+      <Helmet>
+        <title>Home Page</title>
+        <meta name="description" content="Home Page | Description" />
+      </Helmet>
 
-        <Box>
-          <Item slide={0}>
-            <Link to={bmw}>
-              <div>
-                <h5>BMW</h5>
-                <p>
-                  BMW is a German multinational company which currently produces
-                  luxury automobiles and motorcycles, and also produced aircraft
-                  engines until 1945.
-                </p>
-              </div>
-            </Link>
-          </Item>
-          <Item slide={1}>
-            <Link to={ferrari}>
-              <div>
-                <h5>FERRARI</h5>
-                <p>
-                  Ferrari is an Italian luxury sports car manufacturer based in
-                  Maranello. Founded by Enzo Ferrari in 1939 out of Alfa Romeo
-                  race division as Auto Avio Costruzioni, the company built its
-                  first car in 1940.
-                </p>
-              </div>
-            </Link>
-          </Item>
-          <Item slide={2}>
-            <Link to={vw}>
-              <div>
-                <h5>Volkswagen</h5>
-                <p>
-                  Volkswagen is a German automaker founded on 28 May 1937 by the
-                  German Labour Front under Adolf Hitler and headquartered in
-                  Wolfsburg. It is the flagship marque of the Volkswagen Group,
-                  the largest automaker by worldwide sales in 2016 and 2017.
-                </p>
-              </div>
-            </Link>
-          </Item>
-          <Item slide={3}>
-            <Link to={audi}>
-              <div>
-                <h5>Audi</h5>
-                <p>
-                  Audi is a German automobile manufacturer that designs,
-                  engineers, produces, markets and distributes luxury vehicles.
-                  Audi is a member of the Volkswagen Group and has its roots at
-                  Ingolstadt, Bavaria, Germany. Audi-branded vehicles are
-                  produced in nine production facilities worldwide.
-                </p>
-              </div>
-            </Link>
-          </Item>
-          <Item slide={4}>
-            <Link to={mercedes}>
-              <div>
-                <h5>Mercedes</h5>
-                <p>
-                  Mercedes-Benz is a German global automobile marque and a
-                  division of Daimler AG. The brand is known for luxury
-                  vehicles, buses, coaches, and lorries. The headquarters is in
-                  Stuttgart, Baden-Württemberg.
-                </p>
-              </div>
-            </Link>
-          </Item>
-          <Item slide={5}>
-            <Link to={anotherCar}>
-              <div>
-                <h5>Other</h5>
-                <p>Click to know more about our products.</p>
-              </div>
-            </Link>
-          </Item>
-        </Box>
-      </div>
-    );
-  }
-}
+      <Box>
+        <Item slide={0}>
+          <Link to={bmw}>
+            <div>
+              <h5>BMW</h5>
+              <p>
+                BMW is a German multinational company which currently produces
+                luxury automobiles and motorcycles, and also produced aircraft
+                engines until 1945.
+              </p>
+            </div>
+          </Link>
+        </Item>
+        <Item slide={1}>
+          <Link to={ferrari}>
+            <div>
+              <h5>FERRARI</h5>
+              <p>
+                Ferrari is an Italian luxury sports car manufacturer based in
+                Maranello. Founded by Enzo Ferrari in 1939 out of Alfa Romeo
+                race division as Auto Avio Costruzioni, the company built its
+                first car in 1940.
+              </p>
+            </div>
+          </Link>
+        </Item>
+        <Item slide={2}>
+          <Link to={vw}>
+            <div>
+              <h5>Volkswagen</h5>
+              <p>
+                Volkswagen is a German automaker founded on 28 May 1937 by the
+                German Labour Front under Adolf Hitler and headquartered in
+                Wolfsburg. It is the flagship marque of the Volkswagen Group,
+                the largest automaker by worldwide sales in 2016 and 2017.
+              </p>
+            </div>
+          </Link>
+        </Item>
+        <Item slide={3}>
+          <Link to={audi}>
+            <div>
+              <h5>Audi</h5>
+              <p>
+                Audi is a German automobile manufacturer that designs,
+                engineers, produces, markets and distributes luxury vehicles.
+                Audi is a member of the Volkswagen Group and has its roots at
+                Ingolstadt, Bavaria, Germany. Audi-branded vehicles are produced
+                in nine production facilities worldwide.
+              </p>
+            </div>
+          </Link>
+        </Item>
+        <Item slide={4}>
+          <Link to={mercedes}>
+            <div>
+              <h5>Mercedes</h5>
+              <p>
+                Mercedes-Benz is a German global automobile marque and a
+                division of Daimler AG. The brand is known for luxury vehicles,
+                buses, coaches, and lorries. The headquarters is in Stuttgart,
+                Baden-Württemberg.
+              </p>
+            </div>
+          </Link>
+        </Item>
+        <Item slide={5}>
+          <Link to={anotherCar}>
+            <div>
+              <h5>Other</h5>
+              <p>Click to know more about our products.</p>
+            </div>
+          </Link>
+        </Item>
+      </Box>
+    </div>
+  );
+};
 
 HomePage.propTypes = {
   match: PropTypes.object.isRequired,
 };
 
-export default HomePage;
+export default React.memo(HomePage);
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
@@ -9,64 +9,52 @@ const SliderStyled = styled.div`
   max-width: ${props => props.size || '100%'};
 `;
 
-// TODO: use hooks
-class Slider extends React.PureComponent {
-  state = {
-    currentImageIndex: 0,
-  };
+const Slider = ({ fotos, size }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  switchImage = e => {
+  const switchImage = e => {
     const role = +e.target.dataset.role;
-    const fotosLength = this.props.fotos.length;
-    const { currentImageIndex } = this.state;
 
     if (currentImageIndex === 0 && role === -1) {
-      this.setState({ currentImageIndex: fotosLength - 1 });
-    } else if (currentImageIndex === fotosLength - 1 && +role === 1) {
-      this.setState({ currentImageIndex: 0 });
+      setCurrentImageIndex(fotos.length - 1);
+    } else if (currentImageIndex === fotos.length - 1 && +role === 1) {
+      setCurrentImageIndex(0);
     } else {
-      this.setState({
-        currentImageIndex: currentImageIndex + role,
-      });
+      setCurrentImageIndex(currentImageIndex + role);
     }
   };
 
-  render() {
-    const { fotos, size } = this.props;
-    const { currentImageIndex } = this.state;
-
-    return (
-      <SliderStyled size={size}>
-        <div className="d-flex justify-content-between align-items-center">
-          <div>FOTOS: {`${currentImageIndex + 1}/${fotos.length}`}</div>
-          <div className="d-flex">
-            <Button
-              disabled={fotos.length === 1}
-              className="pl-2"
-              size="sm"
-              onClick={this.switchImage}
-              dataset={{ 'data-role': '-1' }}
-              name="Back"
-            />
-            <Button
-              disabled={fotos.length === 1}
-              className="pl-2"
-              size="sm"
-              onClick={this.switchImage}
-              dataset={{ 'data-role': '1' }}
-              name="Next"
-            />
-          </div>
+  return (
+    <SliderStyled size={size}>
+      <div className="d-flex justify-content-between align-items-center">
+        <div>FOTOS: {`${currentImageIndex + 1}/${fotos.length}`}</div>
+        <div className="d-flex">
+          <Button
+            disabled={fotos.length === 1}
+            className="pl-2"
+            size="sm"
+            onClick={switchImage}
+            dataset={{ 'data-role': '-1' }}
+            name="Back"
+          />
+          <Button
+            disabled={fotos.length === 1}
+            className="pl-2"
+            size="sm"
+            onClick={switchImage}
+            dataset={{ 'data-role': '1' }}
+            name="Next"
+          />
         </div>
-        <Image src={fotos[currentImageIndex]} size={size} />
-      </SliderStyled>
-    );
-  }
-}
+      </div>
+      <Image src={fotos[currentImageIndex]} size={size} />
+    </SliderStyled>
+  );
+};
 
 Slider.propTypes = {
   fotos: PropTypes.array,
   size: PropTypes.string,
 };
 
-export default Slider;
+export default React.memo(Slider);
