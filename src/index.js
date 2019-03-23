0e5227b90016258de52a7f7b7229c3e9d41c99e1
remkst777 <31829395+remkst777@users.noreply.github.com>
2019-03-24 01:38:28 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
 
 import AppRouting from 'components/AppRouting';
 import AccountProvider from 'containers/AccountProvider';
@@ -14,8 +13,6 @@ import configureStore from './configureStore';
 
 import './reset-styles.css';
 import './global-styles.css';
-
-window.$ = $;
 
 const store = configureStore();
 
